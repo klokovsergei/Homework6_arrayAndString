b02f7988c3d9803eae880649ee7abddb29d9d6bf
Sergei Klokov <s2584835@ya.ru>
2024-02-03 11:01:27 +0300

    feat: solve task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+namespace task1
+{
+    class Task2
+    {
+        static void WriteText(string str)
+        {
+            Console.Write(str);
+        }
+
+        static string CreateLowStringFromString(string oldStr)
+        {
+            return oldStr.ToLower();
+        }
+
+        static void Main(string[] args)
+        {
+            WriteText("Введите произвольный текс латинскими буквами в разных регистрах:\n");
+            string str = Console.ReadLine()!;
+            
+            WriteText("Этот же текст только в нижнем регистре:\n");
+            WriteText(CreateLowStringFromString(str));
+
+        }
+
+    }
+}
